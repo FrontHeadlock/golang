084fe2e27351e67815fbbd75dbf83eee232a353a
Dobby19 <southvi1@naver.com>
2026-04-17 20:24:05 +0900

    feat: func.go

--- basics/func.go
+++ basics/func.go
@@ -3,11 +3,56 @@ package main
 import "fmt"
 
 func main() {
-	xs := []float64{98, 93, 77, 82, 83}
-	fmt.Println(avg(xs))
+	x, y := f()
+	println(x, y)
+
+	fmt.Println(add(1, 2, 3))
+
+	// 클로저
+	kkk := func(x, y int) int {
+		return x + y
+	}
+
+	fmt.Println(kkk(1, 1))
+
+	//지연
+	defer two()
+	one()
+
+	// recover : runtime 패닉 중단하고, panic 호출에 전달된 값 반환
+	defer func() {
+		str := recover()
+		fmt.Println(str)
+	}()
+	// 패닉 : 런타임오류 raise
+	panic("PANIC")
 }
 
-func avg(xs []float64) float64 {
-	// runtime 오류 raise 용
-	panic("Not Implemented")
+// 다중값 반환
+func f() (int, int) {
+	return 5, 6
+}
+
+// 가변함수
+func add(args ...int) int {
+	total := 0
+	for _, v := range args {
+		total += v
+	}
+	return total
+}
+
+// 재귀함수
+func fact(x uint) uint {
+	if x == 0 {
+		return 0
+	}
+	return x * fact(x-1)
+}
+
+func one() {
+	fmt.Println("one")
+}
+func two() {
+	fmt.Println("two")
 }
